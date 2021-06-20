8116ee6ba5f25722079c899847e7eb2a039c70e6
foofx <fangxuan.foo@gmail.com>
2021-06-20 16:47:51 +0800

    Updated Codes with Comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,3 @@
-//incorporates the JavaScript and DOM Manipulation for date and city input
-//then filteres according to the 2 inputs
-
 // from data.js
 var tableData = data;
 //console.log(tableData);
@@ -28,10 +25,8 @@ writeData(tableData);
 
 //creating more variables with D3 for input and data manipulation
 var datetime_form = d3.select("#datetime");
-var city_form = d3.select("#city");
 
 //for action when form is sumbitted
-city_form.on("submit", runEnter);
 datetime_form.on("submit",runEnter);
 
 //for action when button is clicked
@@ -46,47 +41,33 @@ function runEnter() {
     
     //grabbing the inputs
     var inputDate = d3.select("#datetime"); 
-    var inputCity = d3.select("#city");
 
     //assigning the inputs with the value
     var DateInput = inputDate.property("value");
-    var CityInput = inputCity.property("value");
   
     // console.log(DateInput);
-    // console.log(CityInput);
 
-    //using the inputs for the filter(s)
+    //using the date input for the filter
     var filteredDate = tableData.filter(tableData => tableData.datetime === DateInput);
-    var filteredCity = tableData.filter(tableData => tableData.city === CityInput);
-    var filterCombined = tableData.filter(tableData => tableData.datetime === DateInput && tableData.city === CityInput);
-  
+
     //quality checks
     // console.log(filteredDate);
-    // console.log(filteredCity);
-    // console.log(filteredCombined);
-    // document.getElementById("ufo-data").innerHTML = "";
-
-
+  
     ufotable.html(""); //clearing the tbody element
 
     //declaring response as let for the current block statement
 let response = {
-    filteredDate, filteredCity, filterCombined
+    filteredDate
 }
 
-//conditionals to check if input is valid
-if(response.filterCombined.length !== 0) {
-    writeData(filterCombined); //populate the table if both date and city are entered and matched
+//conditionals to check if Date Input is valid
+if(response.filteredDate.length !== 0) {
+    writeData(filteredDate); //populate the table with the date
 }
-
-
-else if(response.filterCombined.length === 0 && ((response.filteredDate.length !== 0 || response.filteredCity.length !== 0))) {
-    writeData(filteredDate) || writeData(filteredCity); //populate the table if either date or city are entered and matched
-    }
 
 else { //nothing entered matched, hence the table is cleared and page is automatically refreshed in 10 seconds
         ufohead.html(""); //clearing headers then include the notes
-        ufohead.append("tr").append("td").text(`No sightings on ${DateInput || CityInput} ... The Truth is still out there.`);
+        ufohead.append("tr").append("td").text(`No sightings on ${DateInput} ... The Truth is still out there.`);
 
         //following function is to reload the page
         function timedRefresh(timeoutPeriod) {
